4d4c92db639aef593e23d8783797636c51406309
wood20q <quickchad@icloud.com>
2026-09-10 09:25:01 -0500

    Enhance README and add SVG logos; update HTML structure and styling for improved layout and functionality

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # simple_site
-This is a simple, one page site for a class assignment
+
+This is a simple, one page site for a class assignment. This project is based off of a project I am working on allowing students to schedule their classes and work together with advisors and registrar office at their school.
+
+### Disclaimers
+
+All of the .svg files were created by google gemini
--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+// Download Button Events
 const downloadButton = document.getElementById('downloadButton');
 
 downloadButton.addEventListener('click', () => {
-  alert('Download button clicked');
+  downloadButton.textContent = "Downloading..."
+  // alert('Download button clicked');
+})
+
+// Form Submission Events
+const form = document.getElementById("contactForm");
+const formMessage = document.getElementById("formMessage");
+
+form.addEventListener("submit", (event) => {
+  console.log("event =", event)
+  formMessage.style.display = "none";
+  event.preventDefault();
+
+  const name = document.getElementById("nameInput").value;
+  const email = document.getElementById("emailInput").value;
+  const message = document.getElementById("messageInput").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill out all of the fields before submitting the contact form.");
+    return;
+  }
+
+  console.log(`Form submitted: name=${name}, email=${email}, message=${message}`);
+
+  form.reset();
+  formMessage.textContent = `Thank you for your message ${name}! We will get back to you soon at the email address you provided: ${email}.`;
+  formMessage.style.display = "block";
 })
